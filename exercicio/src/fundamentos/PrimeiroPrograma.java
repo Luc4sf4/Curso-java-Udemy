131db576a1660ec343b0479ca26a443302d8462d
Luc4sf4 <superlucas2005@gmail.com>
2024-05-13 16:24:26 -0300

    first commit

---
+++ exercicio/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,32 @@
+package fundamentos;
+
+/**
+ * 
+ * @author Lucas Fernandes Dias <fernandes.dias.lucas2005@gmail.com>  ESSE AQUI É O JAVA DOC
+ * @since JDK1.0
+ * @see
+ */
+public class PrimeiroPrograma {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//Uma Setença de código termina com ;
+		//mais um comentário
+		//fin
+		System.out.println("Primeiro programa parte #01! ");
+		
+		/*
+		 * Linha 1
+		 * Linha 2
+		 * Linha 3
+		 * */
+		
+		System.out.println("Primeiro programa Parte #02! ");
+		
+		System.out.println("Fim");//Aqui tb funciona
+	}
+}

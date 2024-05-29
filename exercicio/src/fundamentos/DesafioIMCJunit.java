fe2f3a27db96e27ee47059a18699ff1d645ff6e4
Luc4sf4 <superlucas2005@gmail.com>
2024-05-29 17:01:16 -0300

    New exercise for java

---
+++ exercicio/src/fundamentos/DesafioIMCJunit.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DesafioIMCJunit {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

2b9e124087d213a418b273db6f1c072e92f21e59
Luc4sf4 <superlucas2005@gmail.com>
2024-08-22 15:26:03 -0300

    Curso java

--- exercicio/src/classe/Data.java
+++ exercicio/src/classe/Data.java
@@ -7,23 +7,39 @@ public class Data {
 	int ano;
 
 	Data() {
-		dia = 1;
-		mes = 1;
-		ano = 1970;
-	}
+//		dia = 1;
+//		mes = 1;
+//		ano = 1970;
 
-	Data(int diaInicial, int mesInicial, int anoInicial) {
-		dia = diaInicial;
-		mes = mesInicial;
-		ano = anoInicial;
+		// this como metodo só poder usar dentro de outro construtor e serve pra chamar
+		// de um construtor para
+		// para outro construtor
+		this(1, 1, 1970);
+	}	
+
+	// o "this." referenciar o objeto que esta sendo criado naquele momento
+	// quando usar ? se usa para referenciar atributos da classe
+	Data(int dia, int mes, int ano) {
+
+		// como acessar a variavel de instancia ?
+		this.dia = dia;// pega o valor que recebeu como parametro e altera a variavel de instancia
+		this.mes = mes;
+		this.ano = ano;
 	}
 
 	String obterDataFormatada() {
-		return String.format("%d/%d/%d", dia, mes, ano);
+		// final define se a variavel é local
+		// varaivel local == variacel declarada dentro do corpo de um metodo
+		final String formato = "%d/%d/%d";// variavel local
+
+		return String.format(formato, dia, mes, ano);
 	}
 
 	void imprimirDataFormatada() {
+		
 		System.out.println(obterDataFormatada());
 	}
-
+	//pertence a instancia então pode-se ser usado em todos os métodos
+	//String formato = "%d/%d/%d";
+	
 }

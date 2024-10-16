884899c1954eef25d24db0d80e81a7a32dc33278
Luc4sf4 <superlucas2005@gmail.com>
2024-10-16 13:14:27 -0300

    Classe Campo: abrir #01

--- campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -3,6 +3,8 @@ package br.com.cod3r.cm.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.cod3r.cm.excecao.ExplosaoException;
+
 public class Campo {
 
 	private final int linha;
@@ -14,12 +16,12 @@ public class Campo {
 
 	private List<Campo> vizinhos = new ArrayList<Campo>();
 
-	public Campo(int linha, int coluna) {
+	Campo(int linha, int coluna) {
 		this.linha = linha;
 		this.coluna = coluna;
 	}
 
-	public boolean adicionarVizinho(Campo vizinho) {
+	boolean adicionarVizinho(Campo vizinho) {
 		boolean linhadiferente = linha != vizinho.linha;
 		boolean colunaDiferente = coluna != vizinho.coluna;
 		boolean diagonal = linhadiferente && colunaDiferente;
@@ -38,23 +40,37 @@ public class Campo {
 			return false;
 		}
 
-//		boolean linhadiferente = linha != vizinho.linha;
-//		boolean colunaDiferente = coluna != vizinho.coluna;
-//		boolean diagonal = linhadiferente && colunaDiferente;
-//
-//		int deltaLinha = Math.abs(linha - vizinho.linha);
-//		int deltaColuna = Math.abs(coluna - vizinho.coluna);
-//		int deltaGeral = deltaColuna + deltaLinha;
-//
-//		if (deltaGeral == 1 && !diagonal) {
-//			vizinhos.add(vizinho);
-//			return true;
-//		} else if (deltaGeral == 2 && diagonal) {
-//			vizinhos.add(vizinho);
-//			return true;
-//		} else {
-//			return false;
-//		}
+	}
+
+	void alterMarcacao() {
+		if (!aberto) {
+			marcado = !marcado;
+		}
+	}
+
+	boolean abrir() {
+
+		if (!aberto && !marcado) {
+			aberto = true;
+			
+			if (minado) {
+				throw new ExplosaoException();
+			}
+
+			if (vizinhaSegura()) {
+				vizinhos.forEach(v -> v.abrir());
+			}
+			
+			return true;
+		}else{
+			return false;
+		}
+
+		
+	}
+
+	boolean vizinhaSegura() {
+		return vizinhos.stream().noneMatch(v -> v.minado);
 	}
 
 }

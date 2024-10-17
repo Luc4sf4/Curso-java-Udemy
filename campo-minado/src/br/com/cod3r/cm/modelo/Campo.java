c9b4e34334ef7f45a8197e03f7a116a0f8399e50
Luc4sf4 <superlucas2005@gmail.com>
2024-10-17 14:43:28 -0300

    Classe Campo: Outros Metodos

--- campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -38,7 +38,7 @@ public class Campo {
 			return true;
 		} else {
 			return false;
-		} 
+		}
 	}
 
 	void alternarMarcacao() {
@@ -71,21 +71,60 @@ public class Campo {
 		return vizinhos.stream().noneMatch(v -> v.minado);
 	}
 
-	void minar(){
+	void minar() {
 		minado = true;
 	}
-	
-	public boolean isMarcado(){
+
+	public boolean isMarcado() {
 		return marcado;
 	}
-	
-	
-	public boolean isAberto(){
+
+	public boolean isAberto() {
 		return aberto;
 	}
-	
-	public boolean isFechado(){
+
+	public boolean isFechado() {
 		return !isAberto();
 	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+
+	boolean objetivoAlcancado() {
+		boolean desvendado = !minado && aberto;
+		boolean protegido = minado && marcado;
+		return desvendado || protegido;
+	}
+
+	long minasNaVizinhanca() {
+		return vizinhos.stream().filter(v -> v.minado).count();
+	}
+
+	void reiniciar() {
+		aberto = false;
+		minado = false;
+		marcado = false;
+	}
+
+	@Override
+	public String toString() {
+		if(marcado){
+			return "x";
+		}else if(aberto && minado){
+			return "*";
+		}else if(aberto && minasNaVizinhanca() > 0){
+			return Long.toString(minasNaVizinhanca());
+		}else if(aberto) {
+			return "";
+		}else{
+			return "?";
+		}
+	}
+	
 	
 }

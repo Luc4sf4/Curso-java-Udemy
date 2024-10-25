ab3b077f070a2ccc2a3a612819792ad5e64b40c6
Luc4sf4 <superlucas2005@gmail.com>
2024-10-25 13:41:55 -0300

    Componente Botao

--- calculadora/src/br/com/cod3r/calc/visao/Display.java
+++ calculadora/src/br/com/cod3r/calc/visao/Display.java
@@ -1,11 +1,27 @@
 package br.com.cod3r.calc.visao;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
+	
+	private final JLabel label;
 
 	public Display() {
+		
+		setBackground(new Color(46, 49, 50));
+		label = new JLabel("123,56");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("courrier", Font.PLAIN, 30));
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT,10, 25));
+		
+		add(label);
 		
 	}
 	

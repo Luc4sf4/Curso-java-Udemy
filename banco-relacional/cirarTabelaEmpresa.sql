fec1c19db62508b3b20172cc620152f511384f4b
Luc4sf4 <superlucas2005@gmail.com>
2024-11-05 12:05:51 -0300

    Criar Tabela Empresas

---
+++ banco-relacional/cirarTabelaEmpresa.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS empresas(
+    id int unsigned not null,
+    nome varchar(255) not null,
+    cnpj int unsigned,
+    PRIMARY key (id),
+    unique key (cnpj)
+);
+
+--cidade_empresas
+create table IF NOT EXISTS empresas_unidades(
+    empresa_id int unsigned not null,
+    cidade_id int unsigned not null,
+    sede tinyint(1) not null,
+    PRIMARY key (empresa_id, cidade_id)
+);

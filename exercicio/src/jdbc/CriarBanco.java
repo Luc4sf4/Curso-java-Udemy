373ccb8293d3d9d73c3c21b30148645cab648750
Luc4sf4 <superlucas2005@gmail.com>
2024-11-06 10:57:26 -0300

    Criar tabela

--- exercicio/src/jdbc/CriarBanco.java
+++ exercicio/src/jdbc/CriarBanco.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 public class CriarBanco {
 
 	public static void main(String[] args) throws SQLException {
+		
 		final String url = "jdbc:mysql://localhost?verifyServerCertificate=false&useSSL=true";
 		final String usuario = "root";
 		final String senha = "admin";
@@ -15,7 +16,7 @@ public class CriarBanco {
 		Connection conexao = DriverManager.getConnection(url, usuario, senha);
 		
 		Statement stmt = conexao.createStatement();
-		stmt.execute("DROP DATABASE IF EXISTS curso_java");
+		stmt.execute("CREATE DATABASE IF NOT EXISTS curso_java");
 		
 		System.out.println("Banco criado com sucesso");
 		
---
+++ exercicio/src/jdbc/FabricaConexao.java
@@ -0,0 +1,22 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaConexao {
+
+	public static Connection getConexao() {
+		try {
+			final String url = "jdbc:mysql://localhost/curso_java?verifyServerCertificate=false&useSSL=true";
+			final String usuario = "root";
+			final String senha = "admin";
+
+			return DriverManager.getConnection(url, usuario, senha);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e);
+		}
+	}
+
+}

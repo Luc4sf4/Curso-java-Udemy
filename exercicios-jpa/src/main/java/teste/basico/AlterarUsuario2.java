9fe151a4f72ab9bb770d06532bf9bfde018a9d89
Luc4sf4 <superlucas2005@gmail.com>
2024-11-12 08:36:58 -0300

    Alterar Usuario #02

---
+++ exercicios-jpa/src/main/java/teste/basico/AlterarUsuario2.java
@@ -0,0 +1,41 @@
+package teste.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
+public class AlterarUsuario2 {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+		EntityManager em = emf.createEntityManager();
+
+		//começo da transação
+		em.getTransaction().begin();
+		
+		Usuario usuario = em.find(Usuario.class, 7l);
+		usuario.setNome("Leonardo");
+		
+		//não é necessário o merge, pois a consulta feita com um contexto 
+		//transacional, o resultado da consulta
+		//ele é dita como uma Entidade gerenciada
+		
+		//Entidade gerenciada = qualquer mudança feita sincronizada com o
+		//banco de dados, mesmo sem chamar o merge
+		//Resumo: qualquer mudança do objeto o jpa sincroniza com o banco de dados, atualizando os dados
+		
+		//em.merge(usuario);
+		
+		System.out.println("Nome: " + usuario.getNome());
+		
+		//Final da transação
+		em.getTransaction().commit();
+		
+		em.close();
+		emf.close();
+
+	}
+
+}

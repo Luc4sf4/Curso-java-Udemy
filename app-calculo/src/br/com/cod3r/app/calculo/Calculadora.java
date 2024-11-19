07ac1bf049284283a3b79e86bd3aa79c64791677
Luc4sf4 <superlucas2005@gmail.com>
2024-11-19 12:31:18 -0300

    Transitive

--- app-calculo/src/br/com/cod3r/app/calculo/Calculadora.java
+++ app-calculo/src/br/com/cod3r/app/calculo/Calculadora.java
@@ -1,11 +1,15 @@
 package br.com.cod3r.app.calculo;
 
 import br.com.cod3r.app.calculo.interno.OperacoesAritimeticas;
+import br.com.cod3r.app.loggin.Logger;
 
 public class Calculadora {
 	
 	private OperacoesAritimeticas opAritmeticas = new OperacoesAritimeticas();
 	public double soma(double... nums) {
+		Logger.info("Somando...");
 		return opAritmeticas.soma(nums);
 	}
+	
+	
 }
--- app-calculo/src/module-info.java
+++ app-calculo/src/module-info.java
@@ -1,4 +1,5 @@
 module app.calculo {
 	
+	requires transitive app.loggin;
 	exports br.com.cod3r.app.calculo;
 }

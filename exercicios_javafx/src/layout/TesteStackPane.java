921c5e1c31c274d320f0c58bfdd6f781d03a5b24
Luc4sf4 <superlucas2005@gmail.com>
2024-11-28 11:51:12 -0300

    StackPane #02

--- exercicios_javafx/src/layout/TesteStackPane.java
+++ exercicios_javafx/src/layout/TesteStackPane.java
@@ -1,5 +1,6 @@
 package layout;
 
+import javafx.application.Platform;
 import javafx.scene.layout.StackPane;
 
 public class TesteStackPane extends StackPane {
@@ -13,14 +14,33 @@ public class TesteStackPane extends StackPane {
 		Caixa c6 = new Caixa().comTexto("6");
 
 		getChildren().addAll(c1, c2, c3, c4, c5, c6);
-		
+
 		setOnMouseClicked(e -> {
-			if(e.getSceneX() > getScene().getWidth() / 2) {
+			if (e.getSceneX() > getScene().getWidth() / 2) {
 				getChildren().get(0).toFront();
-			}else {
+			} else {
 				getChildren().get(5).toBack();
 			}
 		});
+
+		Thread t = new Thread(() -> {
+			while (true) {
+				try {
+					Thread.sleep(3000);
+
+					Platform.runLater(() -> {
+						getChildren().get(0).toFront();
+					});
+
+				} catch (Exception e) {
+				
+				}
+
+			}
+		});
+
+		t.setDaemon(true);
+		t.start();
 	}
 
 }

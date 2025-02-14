c2155cb3b8c97cb1f02681cfbbb65f457e587e2a
Luc4sf4 <superlucas2005@gmail.com>
2025-02-14 11:20:04 -0300

    Simulando Erros

--- exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/controllers/PrimeiroController.java
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/controllers/PrimeiroController.java
@@ -1,15 +1,28 @@
 package br.com.cod3r.exercicios_sb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PrimeiroController {
 
+	/*
+	 * Anotacoes da aula: Simulando erros
+	 * O Spring pode achar a mesma url mas ira dar erro pois pode estar mapeada pelo tipo htpp suportado 
+	 * ex, path saudacao esta setado como post, mas se eu der um get no navegador, ira dar erro, pois nao e o metodo suportado
+	 **/
+	
 //	@RequestMapping(method = RequestMethod.GET, path ="/ola" )
-	@GetMapping(path = { "/ola", "/saudacao" })
+	@GetMapping(path =  "/ola")
 	public String ola() {
 		return "Ola Spring Boot!";
 	}
 
+	@PostMapping(path = "/saudacao" )
+	public String saudacao() {
+		return "Ola Spring Boot! (Post)";
+	}
+
+	
 }

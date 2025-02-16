13709c5d6309fbb334c4ee023d9b022cb653d086
Luc4sf4 <superlucas2005@gmail.com>
2025-02-16 10:48:28 -0300

    Passando Parametros via URL

--- exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/controllers/ClienteController.java
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.cod3r.exercicios_sb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +18,8 @@ public class ClienteController {
 		return new Cliente(28, "Pedro", "123.456.789-00");
 	}
 
+	@GetMapping("/{id}")//buscando cliente por id //PathVariable pode ser usado para pegar as informacoes necessarias que sao passadas pela URL
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Maria", "987.654.321-00" );
+	}
 }

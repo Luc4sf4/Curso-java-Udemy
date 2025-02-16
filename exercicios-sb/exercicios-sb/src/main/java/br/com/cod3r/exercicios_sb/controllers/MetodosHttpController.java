790945e1004f1488f60e1d2e14e9fa2f9a73dc9d
Luc4sf4 <superlucas2005@gmail.com>
2025-02-16 10:40:52 -0300

    Metodos http

---
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/controllers/MetodosHttpController.java
@@ -0,0 +1,41 @@
+package br.com.cod3r.exercicios_sb.controllers;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/metodos")
+public class MetodosHttpController {
+	
+	
+	@GetMapping
+	public String get() {
+		return "Requisicao GET";
+	}
+	
+	@PostMapping
+	public String post() { //usados em formularios
+		return "Requisicao post";
+	}
+	
+	
+	@PutMapping
+	public String put() {
+		return "Requisicao put";
+	}
+	
+	@PatchMapping
+	public String patch() {
+		return "Requisicao patch";
+	}
+
+	@DeleteMapping
+	public String delete() {
+		return "Requisicao delete";
+	}
+}

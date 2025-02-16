7feac1e0d3281809b2369cfae7bfbfca175d4ca6
Luc4sf4 <superlucas2005@gmail.com>
2025-02-16 11:01:57 -0300

    Passando Parametros para WebService e desafio de calculadora Web Service

--- exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/controllers/ClienteController.java
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/controllers/ClienteController.java
@@ -3,23 +3,29 @@ package br.com.cod3r.exercicios_sb.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.exercicios_sb.models.Cliente;
 
 @RestController
-@RequestMapping(path = "/clientes")//ja configura que pra acessar qualquer metodo nesta controller
-								  // tem que colocar o /clientes
+@RequestMapping(path = "/clientes") // ja configura que pra acessar qualquer metodo nesta controller
+									// tem que colocar o /clientes
 public class ClienteController {
-	
-	//retornando objetos como JSON 
+
+	// retornando objetos como JSON
 	@GetMapping(path = "/qualquer")
 	public Cliente obterCliente() {
 		return new Cliente(28, "Pedro", "123.456.789-00");
 	}
 
-	@GetMapping("/{id}")//buscando cliente por id //PathVariable pode ser usado para pegar as informacoes necessarias que sao passadas pela URL
+	@GetMapping("/{id}") // buscando cliente por id //PathVariable busca pela variavel informada na URL
 	public Cliente obterClientePorId1(@PathVariable int id) {
-		return new Cliente(id, "Maria", "987.654.321-00" );
+		return new Cliente(id, "Maria", "987.654.321-00");
+	}
+
+	@GetMapping // RequestParam busca pelo id iunformado como parametro na ur
+	public Cliente obterClientePorId2(@RequestParam(name = "id") int id) {
+		return new Cliente(id, "Joao Agusto", "111.222.333-44");
 	}
 }

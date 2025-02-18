f4e91a42c7262c5eab90f0868292a49b02bdbe5b
Luc4sf4 <superlucas2005@gmail.com>
2025-02-18 17:57:13 -0300

    Consultando Todos os Produtos

--- exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/controllers/ProdutoController.java
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/controllers/ProdutoController.java
@@ -1,9 +1,9 @@
 package br.com.cod3r.exercicios_sb.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.exercicios_sb.model.entities.Produto;
@@ -23,4 +23,9 @@ public class ProdutoController {
 		return produto;
 	}
 
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		return produtoRepository.findAll();
+	}
+	
 }

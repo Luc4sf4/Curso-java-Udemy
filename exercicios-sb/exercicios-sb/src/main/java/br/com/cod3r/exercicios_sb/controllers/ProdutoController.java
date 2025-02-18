4155e8d4923ef297260fb1cc854ad66cf9a5b5d3
Luc4sf4 <superlucas2005@gmail.com>
2025-02-18 17:49:46 -0300

    Aplicando Validacoes Simples

--- exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/controllers/ProdutoController.java
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/controllers/ProdutoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.exercicios_sb.model.entities.Produto;
 import br.com.cod3r.exercicios_sb.model.repositories.ProdutoRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -17,7 +18,7 @@ public class ProdutoController {
 	private ProdutoRepository produtoRepository;
 
 	@PostMapping
-	public Produto novoProduto(Produto produto) {
+	public Produto novoProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
 	}

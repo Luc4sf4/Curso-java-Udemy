becf0478b770dfdcdb4c5d521346edee7999c9fa
Luc4sf4 <superlucas2005@gmail.com>
2025-02-18 18:48:30 -0300

    Mais Consultas & finalizando Modulo

--- exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/model/repositories/ProdutoRepository.java
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/model/repositories/ProdutoRepository.java
@@ -1,7 +1,8 @@
 package br.com.cod3r.exercicios_sb.model.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.cod3r.exercicios_sb.model.entities.Produto;
@@ -10,4 +11,16 @@ public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 	
 	public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
 
+	
+	//outras alternativas
+//	findByNomeContaining
+//	findByNomeIsContaing
+//	findByNomeContains
+//	
+//	findByNomeStartsWith
+//	findByNomeEndsWith
+	
+	@Query("SELECT p FROM Produto p WHERE p.nome LIKE %:nome%")
+	public Iterable<Produto> searchByNameLike(@Param("nome") String nome);
+	
 }

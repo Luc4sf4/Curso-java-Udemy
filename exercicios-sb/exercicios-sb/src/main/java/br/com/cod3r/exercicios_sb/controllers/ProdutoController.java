6ea4b9269af822905c1c8e195cab6aff3a6ba83a
Luc4sf4 <superlucas2005@gmail.com>
2025-02-18 17:34:37 -0300

    Injecao de dependencias e Novos Atributos

--- exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/controllers/ProdutoController.java
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/controllers/ProdutoController.java
@@ -17,11 +17,13 @@ public class ProdutoController {
 	private ProdutoRepository produtoRepository;
 	
 	@PostMapping
-	public Produto novoProduto(@RequestParam String nome) {
-		Produto produto = new Produto(nome);
+	public Produto novoProduto(@RequestParam String nome,
+							   @RequestParam double preco,
+							   @RequestParam double desconto) {
+		Produto produto = new Produto(nome, preco, desconto);
 		produtoRepository.save(produto);
 		return produto;
 	}
-	
+	 
 
 }
